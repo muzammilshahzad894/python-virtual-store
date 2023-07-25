6e74fa9d77ff8416844cff0a70a35faa04851062
Muzammil Shahzad <muzammilshahzad894@gmail.com>
2023-07-25 10:17:24 -0700

    update:allowed-host

--- blog/settings.py
+++ blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'h_-8^%^f8r+@#fg@twc*ts%lnh@f=!b)jj8es=+ki!4&ljc@mo'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['13.53.118.217']
+ALLOWED_HOSTS = ['16.170.56.54']
 
 
 # Application definition
